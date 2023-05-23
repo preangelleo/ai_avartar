a4c81374dffa42f2582ef9636b1480fdee13b85e
yudun <wyudun@gmail.com>
2023-05-22 21:04:18 -0700

    Creat the jupyter service upon boot.

---
+++ src/bot/wechat/wechat_bot.py
@@ -0,0 +1,16 @@
+from src.bot.bot import Bot
+
+
+class WechatBot(Bot):
+    def __init__(self):
+        pass
+
+    def handle_single_msg(self, msg):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    WechatBot().run()

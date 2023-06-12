0e85ce5fafe1c8d226b2d07a9fb043d75718c892
Shimin Wang <wyudun@gmail.com>
2023-06-11 23:24:52 -0700

    Add 2 scripts to help start bots.

---
+++ src/instance_scripts/start_new_telegram_bot.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+export PATH="${PATH}:${HOME}/.local/bin:${HOME}/anaconda3/bin"
+export PYTHONPATH="${PYTHONPATH}:${HOME}/ai_avartar"
+
+cd /root/ai_avartar && conda activate av
+cd /root
+pm2 start ai_avartar/src/bot/fanbook/fanbook_bot.py --name fb --interpreter python3

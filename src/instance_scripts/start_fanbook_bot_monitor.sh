8c1dd6bf6ee84a68d2afc38c235f37865b59048e
ke zhang <kezhang@lyft.com>
2023-06-22 21:20:31 -0700

    rename

---
+++ src/instance_scripts/start_fanbook_bot_monitor.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+export PATH="${PATH}:${HOME}/.local/bin:${HOME}/anaconda3/bin"
+export PYTHONPATH="${PYTHONPATH}:${HOME}/ai_avartar"
+
+cd /root/ai_avartar && conda activate av
+cd /root
+pm2 start ai_avartar/src/bot/telegram/telegram_bot.py --name tg --interpreter python3

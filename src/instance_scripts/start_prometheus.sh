ccfccb95142c74fa5d10434742de35cfe18a2b15
ke zhang <kezhang@lyft.com>
2023-08-11 12:41:53 -0700

    update script to extend it to 1 year

--- src/instance_scripts/start_prometheus.sh
+++ src/instance_scripts/start_prometheus.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 cd /home/ubuntu
-/home/ubuntu/prometheus-2.46.0.linux-amd64/prometheus --config.file=/home/ubuntu/ai_avartar/monitoring/prometheus.yml
+/home/ubuntu/prometheus-2.46.0.linux-amd64/prometheus --config.file=/home/ubuntu/ai_avartar/monitoring/prometheus.yml --storage.tsdb.retention.time=365d --storage.tsdb.retention.size=1GB

2a7836cdeda4e8e0530ac4ebfdc7ca8206a8e921
ke zhang <kezhang@lyft.com>
2023-08-28 22:24:34 -0700

    do not differentiate conversation and drawing

--- src/payments/constant.py
+++ src/payments/constant.py
@@ -4,7 +4,7 @@ SUBSCRIPTION_PLAN = 'subscription_monthty_plan_B'
 PLAN_CONFIG = {
     CREDIT_PLAN_TIER_A: {
         'conversation_credit': 100,
-        'drawing_credit': 10,
+        'drawing_credit': 0,  # we do not differentiate between conversation and drawing credit
     },
     SUBSCRIPTION_PLAN: {
         'plan_duration_days': 30,
@@ -17,7 +17,7 @@ CREDIT_BASED_PLAN = [CREDIT_PLAN_TIER_A]
 SUBSCRIPTION_BASED_PLAN = [SUBSCRIPTION_PLAN]  # they have typo in the callback
 
 PUBLIC_INIT_CHAT_CREDIT = 50
-PUBLIC_INIT_DRAWING_CREDIT = 5
+PUBLIC_INIT_DRAWING_CREDIT = 0
 
 
 class ServiceType:

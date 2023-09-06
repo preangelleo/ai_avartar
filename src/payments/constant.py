d466bf9771fe238713869f596a1e70e6210472fe
ke zhang <kezhang@lyft.com>
2023-09-05 19:57:41 -0700

    change the config

--- src/payments/constant.py
+++ src/payments/constant.py
@@ -1,12 +1,21 @@
 CREDIT_PLAN_TIER_A = 'token_based_plan_a'
-SUBSCRIPTION_PLAN = 'subscription_monthty_plan_B'
+CREDIT_PLAN_TIER_B = 'token_based_plan_b'
+SUBSCRIPTION_PLAN_TEST = 'subscription_monthty_plan_B'  # they have typo in the callback in test
+SUBSCRIPTION_PLAN_PROD = 'subscription_monthly_plan_A'
 
 PLAN_CONFIG = {
     CREDIT_PLAN_TIER_A: {
-        'conversation_credit': 100,
+        'conversation_credit': 50,
         'drawing_credit': 0,  # we do not differentiate between conversation and drawing credit
     },
-    SUBSCRIPTION_PLAN: {
+    CREDIT_PLAN_TIER_B: {
+        'conversation_credit': 50,
+        'drawing_credit': 0,  # we do not differentiate between conversation and drawing credit
+    },
+    SUBSCRIPTION_PLAN_TEST: {
+        'plan_duration_days': 30,
+    },
+    SUBSCRIPTION_PLAN_PROD: {
         'plan_duration_days': 30,
     },
 }
@@ -14,9 +23,9 @@ PLAN_CONFIG = {
 
 CREDIT_BASED_PLAN = [CREDIT_PLAN_TIER_A]
 
-SUBSCRIPTION_BASED_PLAN = [SUBSCRIPTION_PLAN]  # they have typo in the callback
+SUBSCRIPTION_BASED_PLAN = [SUBSCRIPTION_PLAN_TEST, SUBSCRIPTION_PLAN_PROD]  # they have typo in the callback
 
-PUBLIC_INIT_CHAT_CREDIT = 50
+PUBLIC_INIT_CHAT_CREDIT = 15
 PUBLIC_INIT_DRAWING_CREDIT = 0
 
 
